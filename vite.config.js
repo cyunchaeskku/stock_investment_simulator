f95582fb96ca016c6332116247877f3f79eadcae
cyunchaeskku <cyunchaeskku@gmail.com>
2025-11-02 21:32:42 +0900

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-export default {
-  server: {
-    proxy: {
-      '/frankfurter': {
-        target: 'https://api.frankfurter.app',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\\/frankfurter/, ''),
-      },
-    },
-  },
-};
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react()],
+})
